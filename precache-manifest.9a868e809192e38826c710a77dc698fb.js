7d30185e7dee464e15b1a031eb595a0dd6d698a3
Guilherme Pires <mail@gpir.es>
2020-09-17 10:14:06 +0100

    Updates

---
+++ precache-manifest.9a868e809192e38826c710a77dc698fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "144e1a1b2c6e36bff08a83190cc0bf66",
+    "url": "/just-testing-react/index.html"
+  },
+  {
+    "revision": "2dda630d34135d0a3939",
+    "url": "/just-testing-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a2187f352361f7db4644",
+    "url": "/just-testing-react/static/js/2.37ed94fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/just-testing-react/static/js/2.37ed94fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dda630d34135d0a3939",
+    "url": "/just-testing-react/static/js/main.9c84a43d.chunk.js"
+  },
+  {
+    "revision": "6526478116f7ef0c6854",
+    "url": "/just-testing-react/static/js/runtime-main.b796fed1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/just-testing-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
